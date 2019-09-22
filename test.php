5aaa53a1543cab658a65d9258dbd6d93462fb5da
이건호 <gunho7578@gmail.com>
2019-09-22 17:14:26 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,23 @@
 <?php
-    phpinfo();
+    require_once($_SERVER['DOCUMENT_ROOT'] . "include/config/config.inc");
+
+    include_once(CLASS_PATH . "/user.class.lib");
+
+    $userClass = new UserClass();
+
+    $users = $userClass->getUserList2();
+
+    echo "d";
+
+    for ($i = 0; $i<count($users); $i++) {
+        $_idx = $users[$i]['idx'];
+
+        $sql = "insert into device_data(dvi_user) values(";
+        $sql .= $_idx . ")";
+
+        echo $sql . "<br>";
+
+        $DB->Execute($sql);
+    }
+
 ?>
